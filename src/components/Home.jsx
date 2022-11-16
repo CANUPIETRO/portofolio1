5d183d239fec96066020f143c29cd2e71765235e
petros <petroskanou@gmail.com>
2022-11-16 12:55:01 +0200

    init

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {HiArrowCircleRight} from "react-icons/hi";
+
+function Home() {
+  return (
+    <div className='w-full h-screen bg-[#231f20]'>
+        Home
+    </div>
+  )
+}
+
+export default Home
